2a65a2ca0efb60b6eb99da057430e104cdc665ef
HonoravelMacho <tiagorabelosels@gmail.com>
2026-06-10 21:35:41 -0300

    🚚 Add: Script de instalação universal

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Detectar ambiente
+if [ -d "/data/data/com.termux/files/usr/bin" ]; then
+    OS="Android"
+    BIN_DIR="/data/data/com.termux/files/usr/bin"
+    PKG_MGR="pkg install -y"
+else
+    OS="Linux"
+    BIN_DIR="/usr/local/bin"
+    PKG_MGR="sudo apt install -y"
+fi
+
+echo "🔨 Instalando/Atualizando M.A.R.T.E.L.O.S. no $OS..."
+
+# 1. Instalar dependências
+if [ "$OS" = "Linux" ]; then
+    $PKG_MGR libgmp-dev cmake build-essential git
+else
+    $PKG_MGR libgmp cmake clang git
+fi
+
+# 2. Atualizar código
+if [ -d "martelos_temp" ]; then rm -rf martelos_temp; fi
+git clone --depth 1 https://github.com/HonoravelMacho/martelos.git martelos_temp
+cd martelos_temp
+
+# 3. Compilar
+mkdir build && cd build
+cmake ..
+make
+
+# 4. Instalar
+if [ "$OS" = "Linux" ]; then
+    sudo cp martelos $BIN_DIR/
+else
+    cp martelos $BIN_DIR/
+fi
+
+echo "✅ M.A.R.T.E.L.O.S. pronto! Digite 'martelos' para rodar."
+cd ../.. && rm -rf martelos_temp
